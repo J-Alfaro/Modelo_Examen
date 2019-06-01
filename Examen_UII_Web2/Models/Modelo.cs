6b02a55d5ff02e994809f30120229a39accb39bb
Samuel <raseth2.cek22@gmail.com>
2019-06-01 01:10:21 -0500

    Modelo

--- Examen_UII_Web2/Models/Modelo.cs
+++ Examen_UII_Web2/Models/Modelo.cs
@@ -6,6 +6,9 @@ namespace Examen_UII_Web2.Models
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Data.Entity.Spatial;
 
+    using System.Linq;
+    using System.Data.Entity;
+
     [Table("Modelo")]
     public partial class Modelo
     {
@@ -32,5 +35,94 @@ namespace Examen_UII_Web2.Models
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Evidencia> Evidencia { get; set; }
+
+
+
+        //metodo listar
+        public List<Modelo> Listar()//Retorna una coleccion de registros
+        {
+            var objModelo = new List<Modelo>();
+            try
+            {
+                using (var db = new Model_Sistema())
+                {
+                    objModelo = db.Modelo.ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            return objModelo;
+        }
+
+        //metodo obtener
+        public Modelo Obtener(int id)//retorna solo un objeto
+        {
+            var objModelo = new Modelo();
+            try
+            {
+                using (var db = new Model_Sistema())
+                {
+                    objModelo = db.Modelo
+                        .Where(x => x.modelo_id == id)
+                        .SingleOrDefault();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            return objModelo;
+        }
+
+        //metodo guardar
+        public void Guardar()//retorna solo un objeto
+        {
+
+            try
+            {
+                using (var db = new Model_Sistema())
+                {
+                    if (this.modelo_id > 0)
+                    {
+                        //si existe un valor mayor a 0 es porque existe un registro
+                        db.Entry(this).State = EntityState.Modified;
+
+                    }
+                    else
+                    {
+                        //si no existe registro graba(nuevo registro)
+                        db.Entry(this).State = EntityState.Added;
+
+                    }
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+        }
+
+        //metodo Eliminar
+        public void Eliminar()
+        {
+
+            try
+            {
+                using (var db = new Model_Sistema())
+                {
+                    db.Entry(this).State = EntityState.Deleted;
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+        }
     }
 }
